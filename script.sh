b0dc8bb0bb9d49392154e8ae9b3680d58604ffd5
Mohamed Amin Samet <mohamedamin.samet@yandex.com>
2022-12-29 21:48:53 +0100

    update

--- script.sh
+++ script.sh
@@ -3,9 +3,12 @@
 docker pull docker.io/library/redis:7
 docker tag docker.io/library/redis:7 redis
 
+# SERVER_ADDR should be set
+# example:
+# SERVER_ADDR=34.155.67.216:1323
 
 docker build -t server ./server
-docker build -t front ./front
+docker build -t --build-arg=SERVER_ADDR front ./front
 
 
 docker run -d --rm --net=host --name=redis redis
