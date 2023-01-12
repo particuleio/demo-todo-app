3519d88e926a208015d5f5fdc4339feba6d792f6
Mohamed Amin Samet <mohamedamin.samet@yandex.com>
2023-01-12 09:59:42 +0100

    update script

--- src/script.sh
+++ src/script.sh
@@ -5,7 +5,7 @@ docker tag docker.io/library/redis:7 redis
 
 # SERVER_ADDR should be set
 # example:
-export SERVER_ADDR=http://localhost:1323/api
+export SERVER_ADDR=http://34.163.245.68:1323/api
 
 docker build -t front -t docker.io/sametma/front:1 -t docker.io/sametma/front:latest --build-arg=SERVER_ADDR=$SERVER_ADDR ./front
 docker build -t server -t docker.io/sametma/server:1 -t docker.io/sametma/server:latest ./server
@@ -15,8 +15,8 @@ docker run -d --rm --net=host --name=redis redis
 docker run -d --rm --net=host --name=server server
 docker run -d --rm --net=host --name=front front
 
+# docker network create test
 
-
-docker run -d --rm --net=test --name=redis redis
-docker run -d --rm --net=test -p 1323:1323 --name=server server
-docker run -d --rm --net=test -p 8888:8080 --name=front front
+# docker run -d --rm --net=test --name=redis redis
+# docker run -d --rm --net=test --env=DB_ADDR=10.89.0.6:6379  -p 1323:1323 --name=server server
+# docker run -d --rm --net=test -p 8888:8888 --name=front front
